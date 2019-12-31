90716c5d8305d1d971676978e1e19dec169300c7
abhissha <abhishek.spin@gmail.com>
2019-12-31 01:31:56 -0500

    add initial changes

---
+++ LoggingMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleOutput
+{
+        public static class MiddlewareExtensions
+        {
+            public static IApplicationBuilder UseClaimsMiddleware(this IApplicationBuilder builder)
+            {
+                return builder.UseMiddleware<ClaimsMiddleware>();
+            }
+        }
+}
---
+++ Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConsoleOutput.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace ConsoleOutput
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ITokenService, TokenService>();
+            services.AddTransient<ISigningCredentialsService>(x => new SigningCredentialsService(Configuration));
+
+            services.AddAuthentication().AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    IssuerSigningKey = new SigningCredentialsService(Configuration).GetKey()
+                };
+            });
+
+
+            // configure serilog with singleton
+            Log.Logger = new LoggerConfiguration()
+                                .MinimumLevel.Debug()
+                                .Enrich.FromLogContext()
+                                .WriteTo.Console()
+                                .WriteTo.ApplicationInsights(Configuration.GetValue<string>("InstrumentationKey"), TelemetryConverter.Traces)
+                                .CreateLogger();
+
+            services.AddSingleton<Serilog.ILogger>(Log.Logger);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseClaimsMiddleware();
+            app.UseMvc();
+        }
+    }
+}
