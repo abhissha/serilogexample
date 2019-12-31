1f42d493f4aff90a1f05544134fc7370ed7432d7
Abhi Sharma <abhishek.spin@gmail.com>
2019-12-31 14:05:01 -0500

    add autofac

---
+++ httpcontextmiddleware/Extensions/ConfigureAutofacExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HttpContextMiddleware.Extensions
+{
+    public static class ConfigureAutofacExtensions
+    {
+        public static IServiceProvider ConfigureAutoFac(this IServiceCollection services)
+        {
+            var container = new ContainerBuilder();
+            container.Populate(services);
+
+            return new AutofacServiceProvider(container.Build());
+        }
+    }
+}
--- httpcontextmiddleware/Startup.cs
+++ httpcontextmiddleware/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace HttpContextMiddleware
 {
@@ -19,7 +20,7 @@ namespace HttpContextMiddleware
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public virtual IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddHttpContextAccessor();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -27,6 +28,7 @@ namespace HttpContextMiddleware
             services.AddSingleton<ITokenService, TokenService>();
             services.AddTransient<ISigningCredentialsService>(x => new SigningCredentialsService(Configuration));
             services.ConfigureSerilog(Configuration);
+            return services.ConfigureAutoFac();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
